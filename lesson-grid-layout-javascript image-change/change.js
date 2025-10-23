f9210ea1ae1553e6c902a9523f0c144c33a26f42
Kelly-311 <kwilliams3479@students.mchenry.edu>
2025-10-23 10:07:06 -0500

    Add files via upload

---
+++ lesson-grid-layout-javascript image-change/change.js
@@ -0,0 +1,15 @@
+function change01() {
+    document.getElementById("large").src = "images/brothers-change.jpg";
+}
+function change02() {
+    document.getElementById("large").src = "images/flame-change.jpg";
+}
+function change03() {
+    document.getElementById("large").src = "images/statue-change.jpg";
+}
+function change04() {
+    document.getElementById("large").src = "images/sword-change.jpg";
+}
+function change05() {
+    document.getElementById("large").src = "images/furina-change.jpg";
+}
